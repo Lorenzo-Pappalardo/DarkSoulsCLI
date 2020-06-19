4ee31ab4a968327eeb8698744bfb4e2b18c4f424
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-06-19 15:29:39 +0200

    Storage manages I/O

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -3,14 +3,24 @@ package storage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
 
 public class Storage {
     private Storage instance;
-    private FileReader scores;
+    private Reader scoresReader;
 
-    private Storage() throws FileNotFoundException {
+    private Storage() {
+        try {
+            scoresReader = new FileReader(new File("scores.txt"));
+        } catch (FileNotFoundException e) {
+            System.err.println("Couldn't open or create file");
+            System.exit(1);
+        }
+    }
+
+    public Storage getInstance() {
         if (instance == null)
             instance = new Storage();
-        scores = new FileReader(new File("scores.txt"));
+        return instance;
     }
 }
